b7b40ba3bf2f1eddc2c32ea7cac4209c5f5cb3b5
Steven McGinnis <StevenM@yodel.today>
2023-02-28 18:30:29 -0500

    Restart

--- Client.java
+++ Client.java
@@ -1,41 +1,45 @@
 import java.io.*;
 import java.net.*;
 
-public class Client {
-  public static void main(String[] args) throws IOException {
+public class Client 
+{
+  public static void main(String[] args) throws IOException 
+  {
     Socket server = null;
     PrintWriter outToServer = null;
     BufferedReader inFromServer = null;
 
-    try {
+    try 
+    {
       server = new Socket("localhost", 4444);
       outToServer = new PrintWriter(server.getOutputStream(), true);
       inFromServer = new BufferedReader(new InputStreamReader(server.getInputStream()));
-    } catch (UnknownHostException e) {
+    } 
+    catch (UnknownHostException e) 
+    {
       System.err.println("Can't find localhost.");
       return;
-    } catch (IOException e) {
+    } 
+    catch (IOException e) 
+    {
       System.err.println("IO Error on connection to localhost. Is server running?");
       return;
     }
 
     BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
     String userInput;
-    String serverResponse;
-
-    do {
-      // Wait for a response from the server
-      serverResponse = inFromServer.readLine();
-      System.out.println("Server: " + serverResponse);
-
-      // Send input to the server
+    System.out.println("WHOAMI?");
+    do
+    {
       userInput = stdIn.readLine();
       outToServer.println(userInput);
-
-    } while (!userInput.equals("Bye"));
+      System.out.println ("Server: " + inFromServer.readLine ());
+    }
+    while (! userInput.equals ("Bye"));
 
     outToServer.close();
     inFromServer.close();
     server.close();
   }
 }
+
